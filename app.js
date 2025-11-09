0a6dd56af304038ee2dfcf05b81f670a4d35b337
HomamZituni <homam93@msn.com>
2025-11-09 17:22:13 -0500

    writing pseudo code and added boiler plate

---
+++ app.js
@@ -0,0 +1,106 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* Add Task Function pseudo code
+let tasks = [];
+
+function addTask(taskName) {
+  tasks.push(taskName);
+  console.log(`Task "${taskName}" added.`);
+}
+addTask("groceries");
+addTask("walk dog");
+console.log(tasks);
+*/
+
+/* function renderTasks: 
+ clear current task list display
+  for each task in tasks array:
+    create a list item element
+    set its text to the task’s name
+    add it to the task list area
+
+    function renderTasks(tasksarray) { const tasklistelement = document.getElementById('taskList'); 
+    taskListElement.innerHTML = '';
+
+    tasksArray.foreach(function(task))
+    { const li= document.createElement("li");
+     li.textContent = task;
+     taskListElement.appendChild(li);
+      }
+
+
+    */
+
+    /* Update Task Status: 
+    if (new Date(task.deadline) < new Date()) {
+    task.status = "Overdue";
+} */
+
+/* Add Drop Down
+const option = document.createElement("option");
+option.value = "In Progress";
+option.textContent = "In Progress";
+statusDropdown.appendChild(option);
+
+statusDropdown.value = task.status;
+
+statusDropdown.addEventListener("change", (event) => {
+    task.status = event.target.value;
+    renderTasks();
+});
+
+li.appendChild(statusDropdown);
+;
+
+const option = document.createElement("option");
+option.value = "Completed";
+option.textContent = "Completed";
+statusDropdown.appendChild(option);
+
+statusDropdown.value = task.status;
+
+statusDropdown.addEventListener("change", (event) => {
+    task.status = event.target.value;
+    renderTasks();
+});
+
+li.appendChild(statusDropdown);
+;
+
+const option = document.createElement("option");
+option.value = "Overdue";
+option.textContent = "Overdue";
+statusDropdown.appendChild(option);
+
+statusDropdown.value = task.status;
+
+statusDropdown.addEventListener("change", (event) => {
+    task.status = event.target.value;
+    renderTasks();
+});
+
+li.appendChild(statusDropdown);
+;
+
+*/ 
+
+/*  Task Filter 
+function filterTasks(criteria) {
+return tasks.filter(task => task.status == criteria || task.category == criteria);
+}
+*/
+
+
